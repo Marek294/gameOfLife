1101cbd3451473642a7f1d1bc639e66cf2f1dfaa
Marek294 <Markra5835@gmail.com>
2017-08-18 18:32:33 +0200

    Initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+    entry: [
+        'webpack-hot-middleware/client',
+        path.join(__dirname, '/client/index.js')
+    ],
+    output: {
+        path: '/',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: path.join(__dirname, 'client'),
+                loaders: [ 'react-hot-loader', 'babel-loader' ]
+            },
+            {
+                test: /\.scss$/,
+                loaders: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.js' ]
+    }
+}
